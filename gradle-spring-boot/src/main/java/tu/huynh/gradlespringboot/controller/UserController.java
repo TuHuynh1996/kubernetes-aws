ead220b51c17d55f7638b9d9d9251d9dbe5d8bd0
TuHuynh1996 <57967909+TuHuynh1996@users.noreply.github.com>
2024-01-22 01:51:04 +0700

    Add k8s nginx depl and service

--- gradle-spring-boot/src/main/java/tu/huynh/gradlespringboot/controller/UserController.java
+++ gradle-spring-boot/src/main/java/tu/huynh/gradlespringboot/controller/UserController.java
@@ -1,20 +1,13 @@
 package tu.huynh.gradlespringboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import tu.huynh.gradlespringboot.api.UserApi;
 
-@EnableAutoConfiguration
-@SpringBootApplication
-@Configuration
-@ComponentScan
+@RestController
 public class UserController implements UserApi {
 
     @Autowired
